70a39aaa1940a83441ff015e3e20d2580c1fec28
bunny <activebunny@gmail.com>
2017-09-13 08:21:06 -0700

    Update test script

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 sudo insmod kernel_module/npheap.ko
 sudo chmod 777 /dev/npheap
-./benchmark/benchmark 256 256 4
+./benchmark/benchmark 256 8192 4
 cat *.log > trace
-sort -n -t 3 trace > sorted_trace
-./benchmark/validate 256 < sorted_trace
+sort -n -k 3 trace > sorted_trace
+./benchmark/validate 256 8192 < sorted_trace
 rm -f *.log
 sudo rmmod npheap

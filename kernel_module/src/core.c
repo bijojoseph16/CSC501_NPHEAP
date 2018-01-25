9e087306de613e5d16453f9d62ac1ca05856854f
Bijo Joseph <bjoseph4@ncsu.edu>
2018-01-25 14:57:27 -0500

    Copied all code from my NCSU repo

--- kernel_module/src/core.c
+++ kernel_module/src/core.c
@@ -42,11 +42,36 @@
 #include <linux/moduleparam.h>
 #include <linux/poll.h>
 #include <linux/mutex.h>
+#include <asm/io.h>
+#include <linux/radix-tree.h>
 
 extern struct miscdevice npheap_dev;
+//struct to hold the size of an object and its location
+typedef struct object_node {
+    void * address;
+    long size;
+} object;
 
+RADIX_TREE(requested_offsets, GFP_ATOMIC);
 int npheap_mmap(struct file *filp, struct vm_area_struct *vma)
 {
+    object * new_object;
+    void * mem_address;
+    long size = vma->vm_end - vma->vm_start;
+    new_object = (object *)radix_tree_lookup(&requested_offsets, vma->vm_pgoff);
+    if (new_object == NULL) {
+        printk(KERN_WARNING "Offset not requested before\n");
+        mem_address = kmalloc(size, GFP_ATOMIC);
+        new_object = (object *)kmalloc(sizeof(object), GFP_ATOMIC);
+        new_object->size = size;
+        new_object->address = mem_address;
+        radix_tree_insert(&requested_offsets, vma->vm_pgoff, new_object);
+    } else {
+        printk(KERN_WARNING "Offset has been requested previously\n");
+    }
+    if (remap_pfn_range(vma, vma->vm_start, virt_to_phys(new_object->address)>>PAGE_SHIFT, new_object->size, vma->vm_page_prot)) {
+        return -EAGAIN;
+    }
     return 0;
 }
 
--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -43,23 +43,89 @@
 #include <linux/poll.h>
 #include <linux/mutex.h>
 
+//Changes
+#include <linux/radix-tree.h>
+
+//struct to hold the size of an object and its location
+typedef struct object_node {
+    void * address;
+    long size;
+} object;
+
+extern struct radix_tree_root requested_offsets;
+
+//tree for the mutexes
+RADIX_TREE(mutexes, GFP_ATOMIC);
+
 // If exist, return the data.
 long npheap_lock(struct npheap_cmd __user *user_cmd)
 {
+    //check to see if it exists
+    int successful;
+    struct mutex * lock = (struct mutex *)radix_tree_lookup(&mutexes, user_cmd->offset / PAGE_SIZE);
+    printk(KERN_WARNING "locking object %llu\n", user_cmd->offset / PAGE_SIZE);
+    if (lock == NULL) {
+        //not been seen before, so create a new one
+        lock = (struct mutex *) kmalloc(sizeof(struct mutex), GFP_ATOMIC);
+        mutex_init(lock);
+        printk(KERN_WARNING "new lock needed for %llu\n", user_cmd->offset / PAGE_SIZE);
+        //now insert it into the tree
+        //possibility already inserted by different thread, but this won't affect that
+        successful = radix_tree_insert(&mutexes, user_cmd->offset / PAGE_SIZE, lock);
+        if (successful != 0) {
+            printk(KERN_WARNING "lock already created for %llu\n", user_cmd->offset / PAGE_SIZE);
+            //already inserted, so free up this unnecessary memory
+            kfree(lock);
+        }
+    }
+    //guaranteed to be there now
+    printk(KERN_WARNING "getting new lock for %llu\n", user_cmd->offset / PAGE_SIZE);
+    lock = (struct mutex *)radix_tree_lookup(&mutexes, user_cmd->offset / PAGE_SIZE);
+    mutex_lock(lock);    
     return 0;
 }     
 
 long npheap_unlock(struct npheap_cmd __user *user_cmd)
 {
+    //for unlock, we don't need to worry about if it is has been created or not
+    struct mutex * lock = (struct mutex *)radix_tree_lookup(&mutexes, user_cmd->offset / PAGE_SIZE);
+    if (lock != NULL) {
+        printk(KERN_WARNING "unlocking object %llu\n", user_cmd->offset / PAGE_SIZE);
+        mutex_unlock(lock);
+    } else {
+        printk(KERN_WARNING "no lock for ojbect %llu\n", user_cmd->offset / PAGE_SIZE);
+    }
     return 0;
 }
 
 long npheap_getsize(struct npheap_cmd __user *user_cmd)
 {
-    return 0;
+    object * old_object;
+    printk(KERN_WARNING "requesting size of %llu\n", user_cmd->offset / PAGE_SIZE);
+    old_object = (object *)radix_tree_lookup(&requested_offsets, user_cmd->offset / PAGE_SIZE);
+    if (old_object != NULL) {
+        printk(KERN_WARNING "requested size of object known\n");
+        return old_object->size;
+    } else {
+        printk(KERN_WARNING "requested size of object not seen beore\n");
+        return 0;
+    }
 }
 long npheap_delete(struct npheap_cmd __user *user_cmd)
 {
+    //this is the one to do first
+    object * old_object;
+    printk(KERN_WARNING "offset is %llu\n", user_cmd->offset / PAGE_SIZE);
+    old_object = (object *)radix_tree_delete(&requested_offsets, user_cmd->offset / PAGE_SIZE);
+    if (old_object != NULL) {
+        printk(KERN_WARNING "deleting object seen before");
+        if (old_object->address != NULL) {
+            kfree(old_object->address);
+        }
+        kfree(old_object);
+    } else {
+        printk(KERN_WARNING "object not here\n");
+    }
     return 0;
 }
 
